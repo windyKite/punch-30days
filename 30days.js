27bdb3ecd4cae63ea9e59ccba61824b7d898cb0c
萧泳森 <603540681@qq.com>
2018-04-26 23:24:16 +0800

    第1天、第2天

---
+++ 30days.js
@@ -0,0 +1,33 @@
+/**
+ * 第 1 天
+ * 写一个函数 execTime，参数 time：时间毫秒数，作用：什么都不做，但函数执行时会耗时参数传递的毫秒数
+ */
+// function execTime(time){
+//   let currentTime = a = new Date().getTime()
+//   let endTime = currentTime + time
+//   while(currentTime < endTime){
+//     currentTime = new Date()
+//   }
+// }
+// console.log(1)
+// execTime(3000)
+// console.log(2)
+
+/**
+ * 第 2 天
+ * 写一个函数 execTime，参数 t：,时间毫秒数，参数：回调函数，作用：t 秒后执行回调函数
+ */
+// function execTime(t,callback){
+//   setTimeout(callback,t)
+// }
+// console.log(1)
+// execTime(3000,function(){
+//   console.log(3)
+// })
+// console.log(2)
+// 执行结果为：立即输出1和2,3秒后输出3
+
+/**
+ * 第 3 天
+ * 
+ */
