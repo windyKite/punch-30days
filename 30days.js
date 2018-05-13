ca3031907ba64b63a6fb466e2f6a0d97b4c8fd5c
萧泳森 <603540681@qq.com>
2018-05-13 22:34:18 +0800

    第 16 关

--- 30days.js
+++ 30days.js
@@ -530,3 +530,28 @@
 //     console.log(data)
 //   })
 
+/**
+ * 第 16 关
+ * 以下代码输出了什么？为什么？
+ */
+/*
+setTimeout(()=>{
+  console.log(4)
+},0)
+new Promise(resolve => {
+  console.log(1)
+  for(var i = 0;i < 10000; i++){
+    i == 9999 && resolve()
+  }
+  console.log(2)
+}).then(()=>{
+  console.log(5)
+  Promise.resolve(7)
+    .then(v => console.log(v))
+}).then(() => {
+  console.log(6)
+})
+console.log(3)
+*/
+// 输出顺序： 1,2,3,5,6,7,4，主要考察微任务和宏任务的执行顺序。
+
