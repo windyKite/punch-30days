8796ed96fd9306db7e5c5d4b2f0be2300476ef4e
sen <603540681@qq.com>
2018-05-18 15:34:41 +0800

    17关

--- 30days.js
+++ 30days.js
@@ -555,3 +555,43 @@ console.log(3)
 */
 // 输出顺序： 1,2,3,5,6,7,4，主要考察微任务和宏任务的执行顺序。
 
+/**
+ * 第 17 关
+ * 一下代码输出顺序是什么？为什么？
+ */
+/*
+setTimeout(() => {
+  console.log(4)
+  Promise.resolve(8).then(v=>{
+    console.log(v)
+  }).then(()=>{
+    console.log(9)
+  })
+}, 0)
+setTimeout(() => {
+  console.log(10)
+  Promise.resolve(11).then(v=>{
+    console.log(v)
+  }).then(()=>{
+    console.log(12)
+  })
+}, 0)
+new Promise(resolve=>{
+  console.log(1)
+  for(var i = 0; i < 10000;i++){
+    i == 9999 && resolve()
+  }
+  console.log(2)
+}).then(()=>{
+  console.log(5)
+  Promise.resolve(7)
+  .then(v => console.log(v))
+}).then(()=>{
+  console.log(6)
+})
+console.log(3)
+*/
+/**
+ * 顺序： 1，2，3，5，7，6，4，8，9，10，11，12
+ * 先执行宏任务，再执行该宏任务下的微任务，依次循环。
+ */
